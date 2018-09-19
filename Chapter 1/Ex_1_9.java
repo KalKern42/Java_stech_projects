574a0316abbc608ce080695ddcae8bb186e271ee
Kal Kern <student@HSiMac4-96.local>
2018-09-19 10:33:03 -0600

    made Ex_1_9.java for assignment 1-9

---
+++ Chapter 1/Ex_1_9.java
@@ -0,0 +1,13 @@
+/*
+Author: Kal Kern
+Date: 9/19/18
+
+Calculate the area of a rectangle
+*/
+
+class Ex_1_9 {
+	public static void main(String[] args) {
+		System.out.println((4.5 * 2) + (7.9 * 2)); //perimiter
+		System.out.println(4.5 * 7.9); //area
+	}
+}
