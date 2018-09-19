003cd322105b0a5d2eaf56378ffc8d1f986826a2
Kal Kern <student@HSiMac4-96.local>
2018-09-19 10:13:29 -0600

    wrote the 5-1 program: Ex_5_1.java

---
+++ Chapter 1/Ex_1_5.java
@@ -0,0 +1,12 @@
+/*
+Author: Kal Kern
+Date: 9/19/18
+
+gonna do some sweet, sweet math
+*/
+
+class Ex_1_5 {
+	public static void main(String[] args) {
+		System.out.println(((9.5 * 4.5) - (2.5 * 3)) / (45.5 - 3.5)); //this is the expression
+	}
+}
