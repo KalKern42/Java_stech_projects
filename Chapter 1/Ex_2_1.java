7c83386c716edf5e7193d14248b7a7137fd7c3cc
Kal Kern <dictatorofbananasquids@gmail.com>
2018-09-21 08:57:07 -0600

    started Ex_2_1.java

---
+++ Chapter 1/Ex_2_1.java
@@ -0,0 +1,18 @@
+/*
+Author: Kal Kern
+Date: 9/21/18
+
+celsius to fahrenheit conversion
+*/
+import java.util.Scanner;
+
+class Ex_2_1 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		
+		System.out.println("What is the temperature in Celsius? ");
+		double cel = input.nextDouble();
+		
+	}
+}
