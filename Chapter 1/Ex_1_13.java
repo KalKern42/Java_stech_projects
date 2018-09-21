8ceb447d607716e7f3bb6728638c26367e206c1a
Kal Kern <dictatorofbananasquids@gmail.com>
2018-09-21 08:23:49 -0600

    finished 1_13.java

--- Chapter 1/Ex_1_13.java
+++ Chapter 1/Ex_1_13.java
@@ -1,12 +1,18 @@
 /*
 Author: Kal Kern
 Date: 9/19/18
-
+Edited: 9/21/18
 doing a linear equation thing
 */
 
 class Ex_1_13 {
 	public static void main(String[] args) {
-		System.out.println();
+		System.out.println("Your x is: ");  //below is the x equation 
+		System.out.println(((44.5 * 0.55) - (50.2 * 5.9)) / ((3.4 * 0.55) - (50.2 * 2.1)));
+		
+		System.out.println(" ");//space for aesthetic
+		
+		System.out.println("Your y is: "); //below is the y equation
+		System.out.println(((3.4 * 5.9) - (44.5 * 2.1)) / ((3.4 * 0.55) - (50.2 * 2.1)));
 	}
 }
