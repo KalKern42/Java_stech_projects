f3d1857120f0800e666198ed963e3ce0b0c06c5a
TheGreatKalKern <dictatorofbananasquids@gmail.com>
2018-09-25 08:58:53 -0600

    Stared 2-13's program

---
+++ Chapter 2/Ex_2_13.java
@@ -0,0 +1,18 @@
+/*
+Author: Kal Kern
+Date: 9/25/18
+
+is a program that does compound inerest
+*/
+import java.util.Scanner;
+
+class Ex_2_13 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("What is your monthly savings amount? (I promise this is a totaly normal question to ask the humans)");
+		double monthSave = input.nextDouble();
+		
+		monthSave = monthSave * () 
+	}
+}
