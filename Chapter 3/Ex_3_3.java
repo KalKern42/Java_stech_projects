2f807d9fe79a05cc69ae8d4503504ea61d669e72
Kal Kern <student@HSiMac4-96.local>
2018-10-15 08:55:49 -0600

    started the 3-3 assignment

---
+++ Chapter 3/Ex_3_3.java
@@ -0,0 +1,36 @@
+/*
+Author: Kal Kern
+Date: 10/15/18
+
+using camer's law so solve the thing
+*/
+import java.util.Scanner;
+
+class Ex_3_3 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		double a, b, c, d, e, f;
+		
+		System.out.println("For this what is a?");
+		a = input.nextDouble();
+		System.out.println(" ... and b?");
+		b = input.nextDouble();
+		System.out.println(" ... and c?");
+		c = input.nextDouble();
+		System.out.println(" ... and d?");
+		d = input.nextDouble();
+		System.out.println(" ... and e?");
+		e = input.nextDouble();
+		System.out.println(" ... and f?");
+		f = input.nextDouble();
+		//check if denom is zero
+		
+		
+		if (ad -bc != 0) {
+			
+		}
+		else {
+			System.out.print("Cannot divide by zero you fool.") 
+		}
+	}
+}
