0e8ab54e8a274e95bca1a3c41c3b9df4680f9ffa
mwahahahaha <student@HSiMac4-96.local>
2018-11-09 08:58:19 -0700

    started project 4-23 fix rounding next time

---
+++ Chapter 4/Ex_4_23.java
@@ -0,0 +1,54 @@
+/*
+Author: Kal
+Date: 11/9/18
+
+Running a payrole application
+*/
+
+import java.util.Scanner;
+
+class Ex_4_23 {
+	public static void main(String[] args) {
+		Scanner kygrb = new Scanner(System.in); 
+		double hw, hpr, ftw, stw, gp, fw, sw, td, np, prettyftw, prettystw, round; 
+		String nam;
+		
+		//info fetch
+		System.out.print("Enter employee's name: ");
+		nam = kygrb.nextLine();
+		System.out.print("Enter the number of hours worked: ");
+		hw = kygrb.nextDouble();
+		System.out.print("Enter hourly pay rate: $");
+		hpr = kygrb.nextDouble();
+		System.out.print("Enter federal tax witholding rate: ");
+		ftw = kygrb.nextDouble();
+		System.out.print("Enter federal tax witholding rate: ");
+		stw = kygrb.nextDouble();
+		
+		//calculations
+		gp = hw * hpr;
+		fw = gp * ftw;
+		round = Math.rint(fw * 100);
+		fw = round;
+		sw = gp * stw;
+		round = Math.rint(sw * 100);
+		sw = round;
+		td = fw + sw;
+		round = Math.rint(td * 100);
+		td = round;
+		np = gp - td;
+		round = Math.rint(np * 100);
+		np = round;
+		
+		//display
+		System.out.println("Employee's Name: " + nam);
+		System.out.println("Hours worked: " + hw);
+		System.out.println("Pay Rate: $" + hpr);
+		System.out.println("Gross Pay: $" + gp);
+		System.out.println("Deductions:");
+		System.out.println("	Federal Withholding: $" + fw);
+		System.out.println("	State Withholding: $" + sw);
+		System.out.println("	Total Deduction: $" + td);
+		System.out.println("Net Pay: $" + np);
+	}
+}
