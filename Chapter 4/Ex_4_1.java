4dd0d37641bb536f23b47d4f474576566ae8dda9
mwahahahaha <student@HSiMac4-96.local>
2018-11-09 08:08:30 -0700

    Made the code round to 2 decimal points

--- Chapter 4/Ex_4_1.java
+++ Chapter 4/Ex_4_1.java
@@ -11,14 +11,15 @@ class Ex_4_1 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		//variables yay :3
-		Double r, s, area;
+		Double r, s, area, round;
 		 
 		System.out.print("Enter the distance from the center to the vertex: ");
 		r = input.nextDouble();
 		
 		s = (2 * r) * Math.sin(Math.PI / 5); // calculating the side length
 		area = (5 * s * s) / (4 * Math.tan(Math.PI / 5));
-		
+		round = Math.rint(area * 100);
+		area = round/100;
 		System.out.print("The area of your pentagon is " + area);
 		
 	}
