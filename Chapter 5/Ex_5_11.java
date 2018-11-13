83dd93949cb4a0a115270ad274ba4bf7eed65cf7
kal kern <student@HSiMac4-96.local>
2018-11-13 08:59:16 -0700

    started 5-11, needs debugging

---
+++ Chapter 5/Ex_5_11.java
@@ -0,0 +1,29 @@
+/*
+Author: Kal Kern
+Date: 10/13/18
+
+printing out numbers that are divisible by 5 or 6 but noth both
+*/
+import java.util.Scanner;
+
+class Ex_5_11 {
+	public static void main(String[] args) {
+		Scanner kygrb = new Scanner(System.in);
+		int num, xPerLine;
+		
+		num = 100;
+		xPerLine = 0;
+		while (num <= 200) {
+			if (num % 5 == 0 ^ num % 6 == 0) {
+				if (xPerLine <= 10) {
+					System.out.print(num + ", ");
+					xPerLine++;
+				}else {
+					System.out.println("");
+				}
+				
+			}
+			num++;
+		}
+	}
+}
