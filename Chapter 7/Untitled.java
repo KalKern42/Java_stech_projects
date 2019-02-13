aa17cadf347d799fe96989f1227a1f693494f001
404kevnotfound <student@HSiMac4-9.local>
2019-02-13 10:37:10 -0700

    started 7-23

---
+++ Chapter 7/Untitled.java
@@ -0,0 +1,21 @@
+/*
+Author: Kal Kern
+Date: 2/13/19
+
+solving the locker puzzle
+*/
+
+class Untitled {
+	public static void main(String[] args) {
+		Boolean[] lo_ker = new Boolean[100];
+		int s = 1; // students
+		// false = locker is closed
+		// true  = locker is open
+		
+		//creating the array of lockers
+		for (int i = 0; i > 100; i++) {
+			lo_ker[i] = false;
+			}
+		
+	}
+}
